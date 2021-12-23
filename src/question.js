266289a159241c218ec7e00c5e4ccd61bd71deaf
--add <=>
2021-12-23 16:02:28 -0500

    Began react implementation

---
+++ src/question.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    Col,
+    Row,
+    Input,
+    Form,
+    FormGroup,
+    Label,
+    Button,
+    Container,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter
+} from 'reactstrap';
+
+class Question extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content = "",
+
+        }
+    }
+}
